bdf07531cf15c32c413b7f16a69fe321d0c88750
Renato Marques <megadeth.destroys.metallica@gmail.com>
2023-06-19 23:13:45 -0300

    Generated graph and first solution

--- vertex-cover/vertex-cover.py
+++ vertex-cover/vertex-cover.py
@@ -7,17 +7,19 @@ from ortools.linear_solver import pywraplp
 def generate_graph(rng: np.random.Generator, n: int, density: float) -> ig.Graph:
     vertices = np.arange(n)
     adjacency = np.zeros((n, n))
-    max_num_of_edges = int( n * (n - 1) / 2 )
-    for _ in range(density * max_num_of_edges):
+    max_num_of_edges = n * (n - 1) / 2 
+    for _ in range(round(density * max_num_of_edges)):
         while True:
             i, j = rng.choice(vertices, size=2, replace=False)
             if adjacency[i, j] == 0:
                 adjacency[i, j], adjacency[j, i] = 1, 1
                 break
-    return ig.Graph.Adjacency(adjacency, mode="undirected")
+    g = ig.Graph.Adjacency(adjacency, mode="undirected")
+    g.vs['label'] = list(range(n))
+    return g
         
 #%%
-def generate_vertex_cover_relaxation(g: ig.Graph):
+def generate_vertex_cover_problem(g: ig.Graph) -> pywraplp.Solver | None:
     solver = pywraplp.Solver.CreateSolver('GLOP')
     if not solver:
         return
@@ -28,14 +30,36 @@ def generate_vertex_cover_relaxation(g: ig.Graph):
     x = [ solver.IntVar(0, 1, f'x_{i}') for i in range(n)]
 
     adjacency = g.get_adjacency()
-    for i in range(n):
-        for j in range(n - i - 1, -1, -1):
+    for i in range(1, n):
+        for j in range(0, i):
             if adjacency[i][j] == 1:
-                solver.Add(x[i] + x[j] >= 1)
+                if i < j: 
+                    continue
+                solver.Add(x[i] + x[j] >= 1,
+                           f'x_{i} + x_{j} >= 1')
     
-    solver.Minimize( sum(x) )
+    return solver
 
+def copy_solver(solver: pywraplp.Solver) -> pywraplp.Solver:
+    vars: list[pywraplp.Variable] = solver.variables()
+    constraints: list[pywraplp.Constraint] = solver.constraints()
+    version: str = solver.SolverVersion()
+
+#%%
+def solve_bab_vertex_cover(
+        solver: pywraplp.Solver
+):
+    vars: list[pywraplp.Variable] = solver.variables()
+    solver.a
+    
 
 #%%
 gg = generate_graph(np.random.default_rng(2023), 7, 0.5)
 ig.plot(gg)
+solverr = generate_vertex_cover_problem(gg)
+for constraint in solverr.constraints():
+    print(constraint.name())
+solverr.Solve()
+for var in solverr.variables():
+    print(var.solution_value())
+# %%
